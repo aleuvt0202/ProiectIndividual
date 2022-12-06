ae1ce1473baf4112f5dd0d921b7d54cc66a0ed09
Alexandra Cojocari <116676337+aleuvt0202@users.noreply.github.com>
2022-12-06 15:17:30 +0200

    Add files via upload

---
+++ Login.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user(
+   Username        VARCHAR(254)
+  ,Password        VARCHAR(254) 
+  );
+INSERT INTO user(Username, Password) VALUES ('aleuvt11', '12345'),
+INSERT INTO user(Username, Password) VALUES ('andreea1', '6666');
