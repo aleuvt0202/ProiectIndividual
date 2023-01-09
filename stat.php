41cd0f39ed993e18f792cfb23a76c166cba111cd
Alexandra Cojocari <116676337+aleuvt0202@users.noreply.github.com>
2023-01-09 17:45:00 +0200

    Add files via upload
    stat

---
+++ stat.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design1.css">
+    <link rel="stylesheet" href="meniu.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+         
+    <title>GamesWorld</title>
+</head>
+<body style="background: url('wal.png')no-repeat;">
+   <nav>
+    <div class="navbar">
+      <i class='bx bx-menu'></i>
+      <div class="logo"><a href="http://localhost/PI/ok.php">Games World<i class='bx bx-game'></i></a></div>
+      <div class="nav-links">
+        <ul class="links">
+           <li>
+            <a href="http://localhost/PI/stat.php">Statistics</a>
+          </li>
+          <li>
+            <a href="http://localhost/PI/bestgames.php">Best games</a>
+          </li>
+          <li>
+            <a href="http://localhost/PI/history.php">History</a>
+          </li>
+          <li>
+            <a href="http://localhost/PI/signinup1.php">Log out</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<center style="padding-top: 60px; padding-right: 150px;">
+  <div style=" width: 1107px; height: 541.25px; overflow: hidden;">
+    <iframe title="RaportJocuriVideo1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c6ad1248-cdd1-4a76-b17f-e1bb38981194&autoAuth=true&ctid=90bc7298-1c17-48c6-830b-e88b375f216d" frameborder="0" allowFullScreen="true" style="position: relative; bottom: -50px; padding-left: 210px;"></iframe>
+  </div>
+</center>
+
+</body>
+</html>
